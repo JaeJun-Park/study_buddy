e73df629a4b1b34ee30d368ab041a3d756bad277
JaeJun <jejun97@gmail.com>
2022-01-17 18:41:58 +0900

    views & urls & templates are edited

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views # for each urls, connect view page
 
 urlpatterns = [ 
-    path('', views.home, name="home"), # reference a view (specific url) by its name
-    path('room/', views.room, name="room"), 
+    path('', views.home, name="home"), 
+    #2번째 아규먼트 request 오브젝트 받아서 페이지 렌더링하는 함수 주소전달
+    #3번째 아규먼트 reference a view (specific url) by its name
+    path('room/<str:pk>/', views.room, name="room"), 
 ]
 
 
--- studybud/urls.py
+++ studybud/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls), # not yet learn
-    path('', include('base.urls')),
+    path('', include('base.urls')),  # core urls routing만 project의 urls.py가 담당하고 나머지는 app의 urls.py로 위임
 ]
