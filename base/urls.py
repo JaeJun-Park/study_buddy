bde1b732596867972a3bdd912aeba0f656e38c6c
JaeJun <jejun97@gmail.com>
2022-01-22 21:51:33 +0900

    feat: provide updating room feature

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [ # 사용자로 부터 http요청이 들어오면 이 배열 내�
     #2번째 아규먼트 : request 오브젝트 받아서 페이지 렌더링하는 함수 주소전달
     #3번째 아규먼트 : 이 url을 대표하는 이름값을 설정, 템플릿에서 a태그 등으로 경로를 쉽게 연결해주기 위해서 이런 name설정이 필요하다.
     path('room/<str:pk>/', views.room, name="room"), 
-    path('create-room/', views.createRoom, name="create-room")
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room")
 ]
 
 
