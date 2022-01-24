627cfbf2c865ff4d59633f2368ec7cbbc55d3b3f
JaeJun <jejun97@gmail.com>
2022-01-24 22:00:31 +0900

    feat: add search bar feature

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
+
 
 # rooms = [
 #     {'id': 1, 'name':'Lets learn python!'},
@@ -9,12 +11,17 @@ from .forms import RoomForm
 # ] 
 
 # to create a view, we should make function like this
-def home(request): # http object
+def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.all().filter(topic__name__icontains=q)
+    rooms = Room.objects.all().filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
     topics = Topic.objects.all()
-    
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context) # request받아서 'home.html' 템플릿 렌더링해주는 django.shortcuts모듈 속 함수
     
 def room(request, pk): 
